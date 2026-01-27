399ac87e28349b90a55a9dc807f9c05434508a71
Shehbab Kakkar <shehbab1@gmail.com>
2026-01-27 13:33:24 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := NewServer()
+
+	log.Println("Server listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
+
